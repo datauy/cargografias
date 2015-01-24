a42b3a26bd697e9015240f631bf7c0dcb55c4276
Martin Rabaglia <tinchoforever@gmail.com>
2015-01-24 14:33:08 -0300

    mejoras para timeline 3

--- web/new-timeline/app.js
+++ web/new-timeline/app.js
@@ -1,5 +1,72 @@
 angular.module('cargografias', ['ngMaterial', 'ngMessages'])
-.controller('DemoCtrl', function($scope, $mdDialog) {
+.directive('autoComplete',['$http',function($http){
+    return {
+        restrict:'AE',
+        scope:{
+            selectedTags:'=model'
+        },
+        templateUrl:'autocomplete-template.html',
+        link:function(scope,elem,attrs){
+
+            scope.suggestions=[];
+
+            scope.selectedTags=[];
+
+            scope.selectedIndex=-1;
+
+            scope.removeTag=function(index){
+                scope.selectedTags.splice(index,1);
+            }
+
+            scope.search=function(){
+                
+
+            	console.log(scope.searchText);
+            	var data = ["Cristina", "Nestor", "Carlos", "Eduardo", "Fernando"];
+        	    if(data.indexOf(scope.searchText)===-1){
+                    data.unshift(scope.searchText);
+                }
+                scope.suggestions=data;
+                scope.selectedIndex=-1;
+
+                
+            }
+
+            scope.addToSelectedTags=function(index){
+                if(scope.selectedTags.indexOf(scope.suggestions[index])===-1){
+                    scope.selectedTags.push(scope.suggestions[index]);
+                    scope.searchText='';
+                    scope.suggestions=[];
+                }
+            }
+
+            scope.checkKeyDown=function(event){
+                if(event.keyCode===40){
+                    event.preventDefault();
+                    if(scope.selectedIndex+1 !== scope.suggestions.length){
+                        scope.selectedIndex++;
+                    }
+                }
+                else if(event.keyCode===38){
+                    event.preventDefault();
+                    if(scope.selectedIndex-1 !== -1){
+                        scope.selectedIndex--;
+                    }
+                }
+                else if(event.keyCode===13){
+                    scope.addToSelectedTags(scope.selectedIndex);
+                }
+            }
+
+            scope.$watch('selectedIndex',function(val){
+                if(val!==-1) {
+                    scope.searchText = scope.suggestions[scope.selectedIndex];
+                }
+            });
+        }
+    }
+}]
+).controller('DemoCtrl', function($scope, $mdDialog) {
   $scope.user = {
     title: 'Developer',
     email: 'ipsum@lorem.com',
