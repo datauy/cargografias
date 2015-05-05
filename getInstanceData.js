be63ab0ee04b3c230a51048177aa7ee52265b637
Dan Calligaro <dancalligaro@gmail.com>
2015-05-05 01:36:40 -0300

    ahora si

--- getInstanceData.js
+++ getInstanceData.js
@@ -1,6 +1,8 @@
+// npm install request js-beautify
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
+var beautify = require('js-beautify').js_beautify;
 
 function getFiles(instanceName) {
   var files = [
@@ -15,14 +17,14 @@ function getFiles(instanceName) {
     var filename = instanceName + file;
     var url = "http://v3.cargografias.org/js/datasets/gz/" + filename;
     console.log("Getting:", url);
-    request(url, function(error, response, body) {
+    request({url: url, gzip: true}, function(error, response, body) {
       if (error) {
         console.log("Error getting file", filename);
       } else if (response.statusCode != 200) {
         console.log("Error getting file", filename, response.statusCode, response.statusMessage);
       } else {
         var dest = path.normalize("web/js/datasets/gz/" + filename);
-        fs.writeFile(dest, body, function(err) {
+        fs.writeFile(dest, beautify(body), function(err) {
           if (err) console.log("Error writing", dest, err);
           else console.log("Complete:", dest);
         });
