95dffd1a268737aae72a65b3d8956808a621834f
Martin Rabaglia <tinchoforever@gmail.com>
2015-05-06 21:27:32 -0300

    fix para agregar y sacar. Solo nombres y años

--- web/js/lib/v3/cargo-timeline.js
+++ web/js/lib/v3/cargo-timeline.js
@@ -20,9 +20,6 @@ var controls = {
 }
 
 
-
-
-
 function setVisSize() {
 
   wid = $(window).width() - 2;
@@ -36,11 +33,16 @@ function setVisSize() {
 $(window).resize(reloadTimeline);
 
 function reloadTimeline(callback){
-  started = true;
-  setBasicsParams();
-  setVisSize();
+
   // process data for scales, etc.
   processData();
+
+  if (!started){
+    setVisSize();
+    setBasicsParams();
+    started = true;
+  }
+
   drawstarting();
   addInteractionEvents();
 
@@ -65,18 +67,27 @@ function setBasicsParams(){
   
   //remove old svg
   //TODO: is it another way of doing this?
-  d3.select("div.vis svg").remove();
+
 
   vis = d3.select("div.vis")
     .append("svg:svg")
     .attr("class", "vis");
   
+    // background
+  vis.append("svg:rect")
+    .attr("class", "background")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", wid)
+    .attr("height", hei);
+
+
   totals.area = d3.sum(data, function(d) { return d3.sum(d.memberships, function(p){ return p.Land_area_million_km2; }) });
   totals.population = d3.sum(data, function(d) { return d3.sum(d.memberships, function(p){ return p.Estimated_Population; }) });
   
   totals.popPercent = d3.sum(data, function(d) { 
-    if (isNaN(d.Percent_World_Population)) return defaultPopPercent;
-    else return d.Percent_World_Population; 
+  if (isNaN(d.Percent_World_Population)) return defaultPopPercent;
+      else return d.Percent_World_Population; 
     });
   //Append Fonts! 
   vis
@@ -104,7 +115,6 @@ function processData() {
     scales.years = d3.scale.linear()
       .domain([ minYear,maxYear])
       .range([ padding.left, wid - padding.right ]);
-    
 
 
     totalmemberships = d3.sum(data, function(d){return d.memberships.length});
@@ -161,10 +171,6 @@ function processData() {
     //now we order them
     memberships.sort(function(a, b){ return d3.ascending(a, b);});
 
-
-
-
-
     // calculate ordering items
     var y_area = padding.top;
     // Height
@@ -216,23 +222,39 @@ function processData() {
 
 function drawstarting() {
 
-  // background
-  vis.append("svg:rect")
-    .attr("class", "background")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", wid)
-    .attr("height", hei);
+  var visCenter = (wid - padding.left - padding.right) / 2 + padding.left;
 
-  // year ticks
-  vis.selectAll("line")
-    .data(scales.years.ticks(10))
-    .enter().append("svg:line")
-      .attr("class", "tickLine")
-      .attr("x1", padding.left)
-      .attr("x2", padding.left)
-      .attr("y1", padding.top)
-      .attr("y2", hei - padding.bottom);
+  //reload height
+  hei = ($(window).height()/1.5);
+
+  if (controls.height == "memberships")
+  {
+     if(memberships.length > 3){
+      hei = (totalmemberships * boxHeight/2)+75 ;  
+     
+     }else {
+      hei = (totalmemberships* boxHeight) +75;
+    }
+      
+  }
+  else{
+     if(data.length > 3){
+      hei = (data.length * boxHeight)
+     }else {
+      hei = (data.length * boxHeight)+75;
+    }
+  }
+
+  d3.selectAll('.vis')
+    .transition()
+    .attr('width', wid)
+    .attr('height', hei);
+
+  d3.selectAll('.background')
+    .transition()
+    .attr('width', wid)
+    .attr('height', hei);
+
 
   // empire containers
   vis.selectAll("g")
@@ -240,44 +262,7 @@ function drawstarting() {
     .enter()
     .append("svg:g")
       .each(function(politician, j){
-
-        var memberships = d3.select(this)
-        .selectAll('g')
-        .data(politician.memberships);
-        
-        memberships.enter()
-        .append("svg:g")
-        .attr("class", "barGroup")
-        .attr("index", function(d, i) { return j; })
-        .attr("membership", function(d, i) { return i; })
-        
-        .attr("transform", function(d, i) { return "translate(" + (i*100) + ", " + (j*barHeight) + ")"; })
-        .append("svg:rect")
-        .attr("class", function(d) {
-          //TODO: change to type and region?
-          return "bar " + d.post.cargotipo.toLowerCase()  + " " + d.organization.level.toLowerCase() + " " + d.role.toLowerCase();
-        })
-        .attr("x", 0)
-        .attr("y", 0)
-        // .attr("ry", 10)
-        // .attr("rx", 10)
-        .attr("width", function(d) { return scales.years(d.end) - scales.years(d.start); })
-        .attr("height", barHeight)
-         // peak lines    
-        .selectAll("g.barGroup")
-        .append("svg:line")
-        .attr("class", "peakLine")
-        //TODO: What should we do with peaks?
-        //.attr("x1", function(d) { return scales.years(d.Peak) - scales.years(d.start); })
-        //.attr("x2", function(d) { return scales.years(d.Peak) - scales.years(d.start); })
-        .attr("x1", function(d) { return scales.years(d.start) - scales.years(d.start); })
-        .attr("x2", function(d) { return scales.years(d.start) - scales.years(d.start); })
-        
-        .attr("y1", 0)
-        .attr("y2", barHeight);
-
-      
-
+    
         d3.select(this)
           .append('svg:text')
           .attr('class', 'group')
@@ -305,6 +290,15 @@ function drawstarting() {
 
 
 
+  // year ticks
+  vis.selectAll("line")
+    .data(scales.years.ticks(10))
+    .enter().append("svg:line")
+      .attr("class", "tickLine")
+      .attr("x1", padding.left)
+      .attr("x2", padding.left)
+      .attr("y1", padding.top)
+      .attr("y2", hei - padding.bottom);
 
   // tick labels
   vis.selectAll("text.rule")
@@ -317,23 +311,6 @@ function drawstarting() {
       .attr("text-anchor", "middle")
       .text(function(d) { return formatYear(d); });
 
-  
-
-  //axis y dimension. This can be done with any posible feed!  
-  var heightMemberships = (hei - padding.top - padding.bottom) / memberships.length;
-  vis.selectAll('text.membershipLabel')
-    .data(memberships)
-    .enter()
-    .append('svg:text')
-    .attr('class', 'membershipLabel')
-    .attr('dy','.33em')
-    .attr("x", padding.left / 7)
-    .attr("y", function(d,i) { return (i+1.5)*heightMemberships })
-    .text(function(d) {
-      return d;
-    });
-
-
     
 }
 
@@ -343,183 +320,15 @@ function drawstarting() {
 
 function redraw() {
 
-  $("#infobox").hide();
-
-  var visCenter = (wid - padding.left - padding.right) / 2 + padding.left;
-
-  //reload height
-  hei = ($(window).height()/1.5);
-
-  if (controls.height == "memberships")
-  {
-     if(memberships.length > 3){
-      hei = (totalmemberships * boxHeight/2)+75 ;  
-     
-     }else {
-      hei = (totalmemberships* boxHeight) +75;
-    }
-      
-  }
-  else{
-     if(data.length > 3){
-      hei = (data.length * boxHeight)
-     }else {
-      hei = (data.length * boxHeight)+75;
-    }
-  }
-
-  d3.selectAll('.vis')
-    .transition()
-    .attr('height', hei);
-
-  d3.selectAll('.background')
-    .transition()
-    .attr('height', hei);
-
-
-  
-  
-
-  // year ticks
-  vis.selectAll("line.tickLine")
-    .transition()
-      .duration(transitionDuration)
-      .style("opacity", function(d) {
-        //On CarreerMeter hide years. 
-        if (controls.display == "aligned")  return 0;
-        else return 1;
-      })
-      .attr("x1", function(d, i) {
-        if (controls.display == "timeline") return scales.years(d);
-        else if (controls.display == "centered") return visCenter;
-        //On CarreerMeter move years to left
-        else return padding.left;
-      })
-      .attr("x2", function(d) {
-        if (controls.display == "timeline") return scales.years(d);
-        else if (controls.display == "centered") return visCenter;
-        //On CarreerMeter move years to left
-        else return padding.left;
-      })
-      .attr("y1", padding.top)
-      .attr("y2", hei - padding.bottom);
-
-
-
  
-
-  // empire containers
-  vis.selectAll("g.barGroup")
-    .transition()
-      .duration(transitionDuration)
-      .style("fill-opacity", function(d) { 
-        //if (controls.height == "population" && isNaN(d.Percent_World_Population)) return .4;
-        //else 
-        return 1;
-      })
-      .attr("transform", function(d, i) {
-        var tx, ty;
-        //timeline modes.
-        //TimeLine
-        if (controls.display == "timeline") tx = scales.years(d.start);
-        //TODO: What we should do with Peaks
-        //else if (controls.display == "centered") tx = visCenter - (scales.years(d.Peak) - scales.years(d.start));
-        else if (controls.display == "centered") 
-          tx = visCenter - (scales.years(d.start) - scales.years(d.start));
-        
-
-        //Carreer comparsion
-        else if (controls.display == "aligned") {
-          var first = scales.years.ticks()[0];
-          if (d.position=== 0) tx = padding.left;
-          else {
-            
-            
-            
-            tx = d.pre.tx + 
-            //width of the previous
-            (scales.years(d.pre.end)- scales.years(d.pre.start)) +  
-            //distance between previous
-            (scales.years(d.start) - scales.years(d.pre.end))
-          }
-        }
-        
-        
-        
-
-          
-        if (controls.height == "memberships") { 
-            barHeight = (hei - padding.top - padding.bottom) / memberships.length;
-            //Overwrites years
-            //depends on total of type of memberships
-            scales.indexes = d3.scale.linear()
-              .domain([ 0,  memberships.length - 1 ])
-              .range([ padding.top, hei - padding.bottom - barHeight ]);
-
-          ty = scales.indexes(d.membershipsPosition);
-        }
-        else {
-          barHeight = ((hei - padding.top - padding.bottom) / data.length);
-          //depends on politicans
-            scales.indexes = d3.scale.linear()
-                .domain([ 0,  data.length - 1 ])
-                .range([ padding.top, hei - padding.bottom - barHeight]);
-          if (controls.height == "contiguous")  ty = scales.indexes(d.parent);
-          else if (controls.height == "area") ty = d.area_y;
-          else if (controls.height == "population") ty = d.popPercent_y; 
-          else ty = scales.indexes(i);  
-        }
-         
-
-
-        
-
-
-        d.tx = tx;
-        d.ty = ty;
-        return "translate(" + tx + ", " + ty + ")"; 
-      });
-
-
-
-  // bars height! 
-  vis.selectAll("g.barGroup rect.bar")
-    .transition()
-      .duration(transitionDuration)
-      .style("fill-opacity", function(d) { 
-        //if (controls.height == "population" && isNaN(d.Percent_World_Population)) return .25;
-        //else 
-        return 1;
-      })
-      .style('fill', function(d){
-         //Carreer comparsion
-        if (controls.display == "aligned" && controls.height == "contiguous") {
-          return scales.colorsScale(d.parent);
-        }
-        else if (controls.height == "memberships") {
-          return scales.colorsScale(d.membershipsPosition);
-        }
-      })
-      .attr("height", function(d) {
-        if (controls.height == "contiguous") { 
-          return scales.politicians(d.parent);
-        } 
-        else if (controls.height == "area") return scales.areas(d.Land_area_million_km2); 
-        else if (controls.height == "population") return scales.popPercents(d.Percent_World_Population); 
-        else return barHeight;
-      });
-
-    vis.selectAll("svg image")
-        .attr("xlink:href",function(d){ 
-          
-          if (controls.height == "memberships"){ return '';}
-          else return d.image;
-        })
-        .attr('width', 75)
-        .attr('height', 75)
-        .attr("x", padding.left / 7)
-        .attr("y", function(d,i) {return (i)*barHeight/2;})
   
+
+    scales.years = d3.scale.linear()
+      .domain([ minYear,maxYear])
+      .range([ padding.left, wid - padding.right ]);
+
+
+    
     vis.selectAll('svg text.itemLabel')
           .attr("x", padding.left / 7)
           .attr("y", function(d,i) {return (i)*barHeight + (barHeight/2) + padding.top;})
@@ -538,57 +347,7 @@ function redraw() {
 
 
 
- // labels
-  vis.selectAll("g.barGroup")
-    .selectAll("svg text")
-      .attr("class", "barLabel")
-      .attr("x", function(d) { 
-        return 0; })
-      .attr("y", 0)
-      .attr("dx", ".66em")
-      .attr("dy", ".33em")
-      .style("fill", function(d) { if (d.Contiguous === false) return "#0ff"; })
-      .text(function(d) { 
-        //if (d.)
-        if (controls.height == "memberships"){
-          return d.politician.name; //TODO: when do we add the years? + "(" + d.start + "-"+ d.end + ")"  ;   
-        }
-        else {
-          return d.role; //TODO: when do we add the years? + "(" + d.start + "-"+ d.end + ")"  ;   
-        }
-      });
   
-
-
-  // bar labels
-  var labelHeight = 0;
-  vis.selectAll("g.barGroup text.barLabel")
-    .transition()
-      .duration(transitionDuration)
-      .attr("y", function(d) {
-        if (controls.height == "contiguous") { 
-          return scales.politicians(d.parent)/2; 
-        } 
-        else if (controls.height == "area") return scales.areas(d.Land_area_million_km2)/2 - labelHeight; 
-        else if (controls.height == "population") return scales.popPercents(d.Percent_World_Population)/2 - labelHeight; 
-        else return barHeight/2;      
-      });
-  
-
-
-
-  // peak lines
-  vis.selectAll("g.barGroup line.peakLine")
-    .transition()
-      .duration(transitionDuration)
-      .attr("y2", function(d) {
-        if (controls.height == "contiguous") { 
-          return scales.areas(d.Land_area_million_km2); 
-        } 
-        else if (controls.height == "area") return scales.areas(d.Land_area_million_km2); 
-        else if (controls.height == "population") return scales.popPercents(d.Percent_World_Population); 
-        else return barHeight;      
-      });
       
   // tick labels
   vis.selectAll("text.rule")
@@ -599,7 +358,7 @@ function redraw() {
         if (controls.display == "aligned")  return 0;
         else return 1;
       }).attr("x", function(d) {
-        if (controls.display == "timeline") return scales.years(d);
+        if (controls.display == "timeline") { return scales.years(d);}
         else if (controls.display == "centered") return visCenter;
         else return padding.left;     
       })
