d7dc84ab59c8171d50da2ca7ff89738bc2c9f823
Dan Calligaro <dancalligaro@gmail.com>
2015-06-17 04:05:29 +0000

    request on each request

--- server.js
+++ server.js
@@ -31,29 +31,8 @@ MongoClient.connect(mongoUrl, function(err, _db) {
         console.log("cannot connect to db", err);
     } else {
         console.log("Connected correctly to server");
-        reload()
     }
 });
-
-function reload() {
-    return Q.promise(function(resolve, reject, notify) {
-        var instances = db.collection('cargoinstances');
-        instances.find({}).toArray(function(err, instances) {
-            if (err) {
-                reject(err);
-            } else {
-                allInstances = instances;
-                console.log('found', instances.length, 'instances');
-                instancesMap = instances.reduce(function(memo, item) {
-                    memo[item.instanceName] = item;
-                    return memo;
-                }, {});
-                resolve(instances);
-            }
-        });
-    });
-}
-
 
 var app = express();
 
@@ -63,48 +42,48 @@ app.set('views', __dirname + "/views");
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(compression({
-    threshold: 0, // or whatever you want the lower threshold to be
-    filter: function(req, res) {
-        if (req.url.indexOf('gz') > 0) {
-            // res.setHeader( "Content-Encoding", "gzip" );
-        }
-
-    }
-}));
-
-
-function getRouteForInstance(instance) {
-    return function(req, res) {
-        res.render('index', instance);
-    }
-}
-
-app.get('/reload', function(req, res) {
-    reload().then(function() {
-        res.send('ok');
-    }).catch(function(err) {
-        res.status(500).send('cannot reload data');
-    });
-});
 
 app.get('/', function(req, res) {
-    res.render('index', instancesMap.cargografias);
-    reload();
+    req.params.instanceName = 'cargografias';
+    instanceRouteHandler(req,res);
 });
 
-app.get('/:instanceName', function(req, res) {
-    var instance = instancesMap[req.params.instanceName];
-    if(instance){
-        res.render('index', instance)
-        reload();
-    }else{
-        res.status(404).render('instancenotfound');
-    }
-});
+app.get('/:instanceName', instanceRouteHandler);
+        
+function instanceRouteHandler(req, res) {
+
+    var instances = db.collection('cargoinstances');
+    instances.find({instanceName: req.params.instanceName}).toArray(function(err, instances) {
+        if (err) {
+            res.send('error');
+            console.log(err); 
+        } else {
+            if(instances.length > 0){
+                res.render('index', instances[0]) 
+            }else{
+                res.status(404).render('instancenotfound'); 
+            }
+        }
+    });
+
+}
 
 app.get('/d/:instanceName', function(req, res) {
-    res.send(instance)
+
+    var instances = db.collection('cargoinstances');
+    instances.find({instanceName: req.params.instanceName}).toArray(function(err, instances) {
+        if (err) {
+            res.send('error');
+            console.log(err); 
+        } else {
+            if(instances.length > 0){
+                res.send(instances[0]) 
+            }else{
+                res.status(404).render('instancenotfound'); 
+            }
+        }
+    });
+
 });
 
 app.use(express.static(__dirname + '/web'));
