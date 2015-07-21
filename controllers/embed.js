b489bb0313101ef41d0713a4f5577a485510e9be
martin rabaglia <martinrabaglia@martins-MacBook-Pro.local>
2015-07-21 17:48:21 -0300

    this is embed

--- controllers/embed.js
+++ controllers/embed.js
@@ -3,7 +3,7 @@ var request = require('request');
 function index(req, res){
     var id = req.params.id;
     //TODO Load the thing from the DB 
-    res.render('embed', {
+    res.render('embed', { 
         shareUrl: "https://www.cargografias.org/cargo2/embed/1234"
     })
 }
