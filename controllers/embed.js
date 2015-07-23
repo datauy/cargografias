4a51bfab4e81de122f5b3b198ec9d32ff41b8c8d
martin rabaglia <martinrabaglia@martins-MacBook-Pro.local>
2015-07-23 09:49:53 -0300

    embed

--- controllers/embed.js
+++ controllers/embed.js
@@ -4,7 +4,7 @@ function index(req, res){
     var id = req.params.id;
     //TODO Load the thing from the DB 
     res.render('embed', { 
-        shareUrl: "https://www.cargografias.org/cargo2/embed/1234"
+        shareUrl: "https://beta.cargografias.org/cargo2/embed/1234"
     })
 }
 
