1091b11f424197cf49e642ede6d038afc0cf9c56
Dan Calligaro <dancalligaro@gmail.com>
2015-10-20 23:02:39 -0300

    Fix for URLS

--- controllers/fichas.js
+++ controllers/fichas.js
@@ -1,50 +1,100 @@
+var conn = require('../db.js');
 var request = require('request');
 
-function person(req, res){
-	var instanceName = req.params.instanceName;
-	var personId = req.params.personId;
 
-	var url = "https://" + instanceName + ".popit.mysociety.org/api/v0.1/persons/" + personId
+function person(req, res) {
+    var instanceName = req.params.instanceName;
+    var personId = req.params.personId;
+    var instances = conn.getDb().collection('cargoinstances');
+    instances.find({
+        instanceName: req.params.instanceName
+    }).toArray(function(err, instances) {
+        if (err) {
+            console.log(err);
+            res.send({
+                'error': 'error'
+            })
+        } else {
+            if (instances.length > 0) {
+                var url = "https://" + instances[0].popitUrl + "/api/v0.1/persons/" + personId
+                console.log(url);
+                request(url, function(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        try {
+                            body = JSON.parse(body);
+                            res.render('fichaPerson', {
+                                person: body.result,
+                                instanceName: instanceName
+                            })
+                        } catch (ex) {
+                            res.send({
+                                'error': 'error parsing response'
+                            })
+                        }
+                    } else {
+                        res.send({
+                            'error': 'error'
+                        })
+                    }
+                })
+            } else {
+                res.status(404).render('instancenotfound');
+            }
+        }
+    });
 
-	request(url, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	  	try{
-	  		body = JSON.parse(body);
-		    res.render('fichaPerson', {person: body.result, instanceName: instanceName})
-	  	}catch(ex){
-	  		res.send({'error': 'error parsing response'})
-	  	}
-	  }else{
-	  	res.send({ 'error': 'error'})
-	  }
-	})
 
 }
 
-function organization(req, res){
-	var instanceName = req.params.instanceName;
-	var organizationId = req.params.organizationId;
+function organization(req, res) {
+    var instanceName = req.params.instanceName;
+    var organizationId = req.params.organizationId;
 
-	var url = "https://" + instanceName + ".popit.mysociety.org/api/v0.1/organizations/" + organizationId
+    var instances = conn.getDb().collection('cargoinstances');
+    instances.find({
+        instanceName: req.params.instanceName
+    }).toArray(function(err, instances) {
+        if (err) {
+            console.log(err);
+            res.send({
+                'error': 'error'
+            })
+        } else {
 
-	console.log(url);
+            if (instances.length > 0) {
+                var url = "https://" + instances[0].popitUrl + "/api/v0.1/organizations/" + organizationId
 
-	request(url, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	  	try{
-	  		body = JSON.parse(body);
-		    res.render('fichaOrganization', {organization: body.result})
-	  	}catch(ex){
-	  		res.send({'error': 'error parsing response'})
-	  	}
-	  }else{
-	  	res.send({ 'error': 'error'})
-	  }
-	})
+                console.log(url);
+
+                request(url, function(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        try {
+                            body = JSON.parse(body);
+                            res.render('fichaOrganization', {
+                                organization: body.result
+                            })
+                        } catch (ex) {
+                            res.send({
+                                'error': 'error parsing response'
+                            })
+                        }
+                    } else {
+                        res.send({
+                            'error': 'error'
+                        })
+                    }
+                })
+
+            } else {
+                res.status(404).render('instancenotfound');
+            }
+        }
+    });
+
 
 }
 
 module.exports = {
-	person: person, 
-	organization: organization
+    person: person,
+    organization: organization
 }
