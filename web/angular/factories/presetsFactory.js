cc9c35a4ed0396c289e3631ad752849d540b54dc
Tincho <tincho>
2015-11-20 11:40:51 -0300

    agrego grilla magica

--- web/angular/factories/presetsFactory.js
+++ web/angular/factories/presetsFactory.js
@@ -9,19 +9,19 @@ angular.module('cargoApp.factories')
     factory.presets = [];
 
     factory.presets.push({
-      nombre:'Presidentes de la Democracia',
+      nombre:'PresidentesDemocracia',
       valores:'name-466721-df417b-c712cb-e07b04-33d4c4-455e6c-04a407',
     });
     factory.presets.push({
-      nombre:'Candidatos 2015',
+      nombre:'Candidatos2015',
       valores:'name-843c98-43300a-0713d6-a445a1-67fc76-be5cbb',
     });
     factory.presets.push({
-      nombre:'Intendentes Populares',
+      nombre:'IntendentesPopulares',
       valores:'name-689d5f-b96f8d-4da31d-c4f4f0',
     });
     factory.presets.push({
-      nombre:'Gobernadores Reelectos',
+      nombre:'GobernadoresReelectos',
       valores:'name-6c2502-2c487f-818b96-64634b-7b9f45-843c98',
     });
 
